f4a03cc8692a2d3734ed0f9b8173d729db27057d
fusoyeahhh <>
2022-03-29 21:00:18 -0700

    flag_draft: basic draft functionality

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # bc_flag_draft
 Flag Drafting Tools for FFVI BC
+
+## Help
+
+`python -m flag_draft --help`
+
+## Random Flags
+
+`python -m flag_draft -r`
+
+## Draft Flags
+
+`python -m flag_draft -d 5`
--- flag_draft/__main__.py
+++ flag_draft/__main__.py
@@ -1,3 +1,4 @@
+import sys
 import argparse
 import random
 import pandas
@@ -8,21 +9,54 @@ alpha_codes = [{"Code": chr(c), "Effect": None, "IsAlpha": True}
 codes = pandas.concat((codes, pandas.DataFrame(alpha_codes)))
 codes["IsAlpha"] = codes["IsAlpha"].fillna(False)
 
+def show_result(draft_codes):
+    print("Draft complete, flag string follows:")
+    print("".join(draft_codes.loc[draft_codes["IsAlpha"]]["Code"]))
+    print("".join(draft_codes.loc[~draft_codes["IsAlpha"]]["Code"]))
+
 argp = argparse.ArgumentParser()
 argp.add_argument("-s", "--show-flags", action="store_true",
                   help="Show available flags for draft.")
 argp.add_argument("-r", "--randomize-flags", type=int,
                   help="Provided with numerical argument, give that many random flags.")
+argp.add_argument("-d", "--draft-length", type=int, default=5,
+                  help="Provided with numerical argument, do this many draft rounds.")
 args = argp.parse_args()
+
 
 if args.show_flags:
     print(codes)
 
-draft_codes = ""
-alpha_draft_codes = ""
 if args.randomize_flags:
     draft_codes = codes.sample(min(args.randomize_flags, len(codes)))[["Code", "IsAlpha"]]
+    show_result(draft_codes)
+    sys.exit()
 
-print("Draft complete, flag string follows:")
-print("".join(draft_codes.loc[draft_codes["IsAlpha"]]["Code"]))
-print("".join(draft_codes.loc[~draft_codes["IsAlpha"]]["Code"]))
+#
+# Main draft logic
+#
+
+draft_codes = pandas.DataFrame()
+for i in range(args.draft_length):
+    choices = codes.sample(3)
+    print(f"[Round {i+1}] Choices:")
+    j = 1
+    for _, choice in choices.iterrows():
+        print(f"({j}) {choice['Code']}: {choice['Effect']}")
+        j += 1
+
+    while True:
+        try:
+            choice = int(input("choice> "))
+        except ValueError:
+            choice = None
+        if choice not in (1, 2, 3):
+            print("Invalid choice, just 1, 2, or 3 please.")
+            continue
+
+        choice = choices.iloc[choice]
+        draft_codes.append(choice)
+        break
+
+print(draft_codes)
+show_result(draft_codes)
