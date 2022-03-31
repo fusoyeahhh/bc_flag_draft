67d67c88748ce7e0e18de98dfe8f4492057563cf
fusoyeahhh <>
2022-03-30 23:02:27 -0700

    flag_draft: use BC code options / descriptions and add some features

--- flag_draft/__main__.py
+++ flag_draft/__main__.py
@@ -1,7 +1,7 @@
 import sys
 import argparse
-import random
 import pandas
+from . import options
 
 # Available modes:
 """
@@ -13,60 +13,63 @@ import pandas
 6. dragonhunt - Kill all 8 dragons in the World of Ruin. Intended for racing.
 """
 
-# alpha explanations
-_ALPHA = """b Make the game more balanced by removing known exploits.
-c Randomize palettes and names of various things.
-d Randomize final dungeon.
-e Randomize esper spells and levelup bonuses.
-f Randomize enemy formations.
-g Randomize dances
-h Your party in the Final Kefka fight will be random.
-i Randomize the stats of equippable items.
-j Randomize the phantom forest.
-k Randomize the clock in Zozo
-l Randomize blitz inputs.
-m Randomize enemy stats.
-n Randomize window background colors.
-o Shuffle characters' in-battle commands
-p Randomize the palettes of spells and weapon animations.
-q Randomize what equipment each character can wear and character stats.
-r Randomize character locations in the world of ruin.
-s Swap character graphics around.
-t Randomize treasure, including chests, colosseum, shops, and enemy drops.
-u Umaro risk. (Random character will be berserk)
-w Generate new commands for characters, replacing old commands.
-y Shuffle magicite locations.
-z Always have "Sprint Shoes" effect."""
-_ALPHA = {s[0]: s[2:] for s in _ALPHA.split("\n")}
-
-codes = pandas.read_excel("Codes_v5.2.xls")
-alpha_codes = [{"Code": chr(c), "Effect": _ALPHA[chr(c)], "IsAlpha": True}
-                    for c in range(ord("a"), ord("z")) if chr(c) in _ALPHA]
-codes = pandas.concat((codes, pandas.DataFrame(alpha_codes)))
-codes["IsAlpha"] = codes["IsAlpha"].fillna(False)
-codes = codes.reset_index()
-
 def show_result(draft_codes):
     print("Draft complete, flag string follows:")
-    print("".join(draft_codes.loc[draft_codes["IsAlpha"]]["Code"].sort_values()))
-    print("".join(draft_codes.loc[~draft_codes["IsAlpha"]]["Code"]))
+    print("".join(draft_codes.loc[draft_codes["is_flag"]]["name"].sort_values()))
+    print("".join(draft_codes.loc[~draft_codes["is_flag"]]["name"]))
 
 argp = argparse.ArgumentParser()
-argp.add_argument("-s", "--show-flags", action="store_true",
+argp.add_argument("-S", "--show-flags", action="store_true",
                   help="Show available flags for draft.")
+argp.add_argument("-a", "--always-on", action="append",
+                  help="Code will not be presented as an option, "
+                       " and always present in final result. "
+                       "Prefix with '!' to ban code.")
+# TODO: merge with above and use "-" as a removal
+argp.add_argument("-B", "--ban-category", action="append",
+                  help="Code category will not be presented as an option.")
+argp.add_argument("-o", "--only-codes", action="store_true",
+                  help="Only randomize over codes, not flags")
 argp.add_argument("-r", "--randomize-flags", type=int,
                   help="Provided with numerical argument, give that many random flags.")
+argp.add_argument("-s", "--draft-size", type=int, default=3,
+                  help="Provided with numerical argument, do this many choices per round.")
+argp.add_argument("-R", "--draft-rerolls", type=int, default=0,
+                  help="Allow this many rerolls. Default is zero.")
 argp.add_argument("-d", "--draft-length", type=int, default=5,
                   help="Provided with numerical argument, do this many draft rounds.")
 args = argp.parse_args()
 
+codes = pandas.DataFrame(options.NORMAL_CODES)
+alpha_codes = pandas.DataFrame(options.ALL_FLAGS)
+alpha_codes["is_flag"] = True
+alpha_codes["long_description"] = alpha_codes["description"]
+codes = pandas.concat((codes, pandas.DataFrame(alpha_codes)))
+codes["is_flag"] = codes["is_flag"].fillna(False)
+codes = codes.reset_index()
+
+draft_codes = []
+always_on = {code for code in args.always_on if not code.startswith("!")}
+if always_on:
+    turn_on = codes["name"].isin(always_on)
+    draft_codes.extend(codes.loc[turn_on].index)
+    codes = codes.loc[codes.index.difference(turn_on)]
+
+ban_code = {code[1:] for code in args.always_on if code.startswith("!")}
+if ban_code:
+    turn_off = codes["name"].isin(ban_code)
+    codes = codes.loc[codes.index.difference(turn_off)]
+if args.ban_category:
+    codes = codes.loc[~codes["category"].isin(args.ban_category)]
+if args.only_codes:
+    codes = codes.loc[~codes["is_flag"]]
 
 if args.show_flags:
     print(codes)
     sys.exit()
 
 if args.randomize_flags:
-    draft_codes = codes.sample(min(args.randomize_flags, len(codes)))[["Code", "IsAlpha"]]
+    draft_codes = codes.sample(min(args.randomize_flags, len(codes)))[["name", "is_flag"]]
     show_result(draft_codes)
     sys.exit()
 
@@ -74,26 +77,35 @@ if args.randomize_flags:
 # Main draft logic
 #
 
-draft_codes = []
-for i in range(args.draft_length):
-    choices = codes.loc[codes.index.difference(draft_codes)].sample(3)
+rerolls = args.draft_rerolls or 0
+
+i = 0
+while len(draft_codes) < args.draft_length + len(args.always_on or []):
+    pool = codes.index.difference(draft_codes)
+    choices = codes.loc[pool].sample(args.draft_size)
+
     print(f"[Round {i+1}] Choices:")
     j = 1
     for _, choice in choices.iterrows():
-        print(f"({j}) {choice['Code']}: {choice['Effect']}")
+        print(f"({j}) {choice['name']}: {choice['long_description']}")
         j += 1
+    if rerolls > 0:
+        print(f"({j}) reroll")
 
     while True:
         try:
             choice = int(input("choice> "))
         except ValueError:
             choice = None
-        if choice not in (1, 2, 3):
+        if rerolls > 0 and choice == 4:
+            break
+        elif choice not in (1, 2, 3):
             print("Invalid choice, just 1, 2, or 3 please.")
             continue
 
         choice = choices.index[choice - 1]
         draft_codes.append(choice)
+        i += 1
         break
 
 draft_codes = codes.loc[draft_codes]
